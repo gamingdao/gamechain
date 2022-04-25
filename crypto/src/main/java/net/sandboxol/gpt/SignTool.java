1612be7d18a3393974ee733f5f7c337281d2373a
政客君 <kejun@zheng.biz>
2022-04-25 12:27:22 +0800

    upgrade import lib to latest

--- crypto/src/main/java/net/sandboxol/gpt/SignTool.java
+++ crypto/src/main/java/net/sandboxol/gpt/SignTool.java
@@ -3,8 +3,8 @@ package net.sandboxol.gpt;
 import java.util.Arrays;
 import java.util.HashMap;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.hhoss.code.ecc.Address;
 import com.hhoss.code.ecc.KeysHolder;
 import com.hhoss.code.ecc.KeysNode;
